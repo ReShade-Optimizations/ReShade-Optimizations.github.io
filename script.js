38803efa1c736e1162bfc57260600485aa8bea03
Atynzx <ii1234lolxd@gmail.com>
2025-03-21 21:57:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Stop default form submission
-    let form = this;
-
-    fetch(form.action, {
-        method: "POST",
-        body: new FormData(form)
-    }).then(() => {
-        document.getElementById("success-message").style.display = "block";
-        form.reset(); // Clear the form
-    }).catch(error => console.error("Error:", error));
-});
-
 function downloadApp() {
     // Scroll to the download section
     document.getElementById("download").scrollIntoView({ behavior: "smooth" });
